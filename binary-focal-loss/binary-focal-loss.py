113b97482dd4710a5746ed271f11a063d49a70dd
Phongwit Srikokcharoen <101680137+bamboo51@users.noreply.github.com>
2026-03-29 20:50:28 +0900

    Update solution for binary-focal-loss

--- binary-focal-loss/binary-focal-loss.py
+++ binary-focal-loss/binary-focal-loss.py
@@ -5,10 +5,16 @@ def binary_focal_loss(predictions, targets, alpha, gamma):
     """
     predictions = np.array(predictions)
     targets = np.array(targets)
+
+    eps = 1e-9
+    predictions = np.clip(predictions, eps, 1 - eps)
+    
     p_t = np.where(
         targets==1, 
         predictions,
         1-predictions
     )
-        
-    return np.mean(-alpha*np.pow((1-p_t), gamma)*np.log(p_t))
+    # in real case: alpha depends on the class:
+    # for positive class → alpha
+    # for negative class → 1 - alpha    
+    return np.mean(-alpha*np.power((1-p_t), gamma)*np.log(p_t))

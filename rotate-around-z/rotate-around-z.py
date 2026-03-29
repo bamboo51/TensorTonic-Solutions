7b5c292488d641d771c06ecb46b362a219003a36
Phongwit Srikokcharoen <101680137+bamboo51@users.noreply.github.com>
2026-03-29 17:02:35 +0900

    Add solution for rotate-around-z

---
+++ rotate-around-z/rotate-around-z.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+def rotate_around_z(points, theta):
+    """
+    Rotate 3D point(s) around the Z-axis by angle theta (radians).
+    """
+    points = np.array(points)
+    rotate_mat = np.array([
+        [np.cos(theta), -np.sin(theta), 0],
+        [np.sin(theta), np.cos(theta), 0],
+        [0, 0, 1]
+    ])
+
+    return points @ rotate_mat.T
